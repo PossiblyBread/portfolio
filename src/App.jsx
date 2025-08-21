3f919415232a222d1784bcf1ff6f0ca881fa142e
PossiblyBread <adrian2zero@gmail.com>
2025-08-21 16:14:34 +0800

    modified:   src/App.jsx 	renamed:    src/assets/cirno.jpg -> src/assets/images/cirno.jpg 	renamed:    src/assets/pearto.jpg -> src/assets/images/pearto.jpg 	deleted:    src/assets/react.svg 	renamed:    src/components/Card.jsx -> src/components/Card/Card.jsx 	renamed:    src/components/Card.module.css -> src/components/Card/Card.module.css 	renamed:    src/components/Footer.jsx -> src/components/Footer/Footer.jsx 	renamed:    src/components/Footer.module.css -> src/components/Footer/Footer.module.css 	renamed:    src/components/Header.jsx -> src/components/Header/Header.jsx 	renamed:    src/components/Header.module.css -> src/components/Header/Header.module.css 	deleted:    src/components/Sidebar.jsx 	new file:   src/components/Sidebar/Sidebar.jsx 	renamed:    src/components/Sidebar.module.css -> src/components/Sidebar/Sidebar.module.css 	modified:   src/index.css 	new file:   src/pages/About.jsx 	new file:   src/pages/Contact.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/Projects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Sidebar from "./components/Sidebar.jsx";
-import Card from "./components/Card.jsx";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Card from "./components/Card/Card.jsx";
 
-import imageTeto from './assets/pearto.jpg';
-import imageCirno from './assets/cirno.jpg';
+import imageTeto from './assets/images/pearto.jpg';
+import imageCirno from './assets/images/cirno.jpg';
 
 function App() {
   return (
     <>
       <Header />
-      <Sidebar />
+      <Sidebar>
+        <Card src={imageCirno} Name="Cirno" Text="Small Card" variant="profile"/>
+      </Sidebar>
       <div className="main-content">
         <Card variant="large">
           <h2>Card Inside Card Example</h2>
