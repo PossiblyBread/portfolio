872db8faf65e8e6d0199d5a0c47a912cf35025e8
PossiblyBread <adrian2zero@gmail.com>
2025-08-23 19:22:31 +0800

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {Header} from "./components/Header/Header.jsx";
-import {Footer} from "./components/Footer/Footer.jsx";
-import {Home} from "./pages/Home.jsx";
-import {About} from "./pages/About.jsx";
-// Check Point
+import Header from "./components/Header/Header.jsx";  
+import Footer from "./components/Footer/Footer.jsx";  
+import Home from "./pages/Home.jsx";                 
+import About from "./pages/About.jsx";            
+
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <Header />
       <main className="main-content">
         <Routes>
